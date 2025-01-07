dd0f7959a9ecdb186ca7839d3fe64face99fa5ec
MostafaDarwish93 <MostafaDarwish9392@gmail.com>
2025-01-07 21:39:50 +0200

    feat: :sparkles: add cloudinary and mongo to this project
    cloudinary and monogdb has been added

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+const connectDB = async () => {
+
+    mongoose.connection.on("connected",()=>{
+        console.log("MongoDB connected")
+    })
+    console.log(process.env.MONGODB_URI)
+    await mongoose.connect(`${process.env.MONGODB_URI}/E-commerce`, )
+}
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 
+dotenv.config({path:"./.env"}); // to access the environment variables
 // app config
 const app = express(); // create an instance of express
 const port = process.env.PORT || 9000;
-
+connectDB(); // connect to the database
+connectCloudinary(); // connect to the cloudinary
 // middleware
 
 app.use(express.json()); // to parse the json data
 app.use(cors());// to access the api from any domain
 
 // api endpoints
+app.use('/api/user', userRouter);
 
 app.get('/',(req,res)=>{
     res.status(200).send('Hello World');

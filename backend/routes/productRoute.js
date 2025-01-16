621acf6f6b42f00d448342c0b6f4617bbe687e28
MostafaDarwish93 <MostafaDarwish9392@gmail.com>
2025-01-16 16:04:50 +0200

    refactor: :art: updating product model and route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,14 +1,23 @@
 import express from 'express';
 
-import { addProduct, totalProdcut, deleteProduct, productDetails} from '../controllers/productController.js';
+import { addProduct, totalProdcuts, deleteProduct, productDetails} from '../controllers/productController.js';
+import upload from '../middleware/multer.js';
+import adminAuth from '../middleware/adminAuth.js';
 
 const productRouter = express.Router();
 
-productRouter.post('/add', addProduct);
+productRouter.post('/add',adminAuth,
+    upload.fields([
+        {name:'image1', },
+        {name:'image2', },
+        {name:'image3', },
+        {name:'image4', }
+    ])
+     ,addProduct);
 
-productRouter.get('/total', totalProdcut);
+productRouter.get('/total', totalProdcuts);
 
-productRouter.delete('/delete/:id', deleteProduct);
+productRouter.delete('/delete', adminAuth,deleteProduct);
 
 productRouter.get('/details/:id', productDetails);
 
